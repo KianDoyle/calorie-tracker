178c29d385df303ea112b1a9db3c13567099f421
KianDoyle <kianrk@icloud.com>
2024-11-14 12:22:04 +0000

    Create: created api service and DatabaseService
    - also created tracker entity from database

---
+++ src/main/java/com/kd/calorietracker/entities/Tracker.java
@@ -0,0 +1,68 @@
+package com.kd.calorietracker.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tracker", schema = "tracker_db")
+public class Tracker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "item", nullable = false)
+    private String item;
+
+    @Column(name = "sku", nullable = false, length = 100)
+    private String sku;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+}
---
+++ src/main/java/com/kd/calorietracker/services/DatabaseService.java
@@ -0,0 +1,11 @@
+package com.kd.calorietracker.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+
+    public Tracker getTracker() {
+
+    }
+}
