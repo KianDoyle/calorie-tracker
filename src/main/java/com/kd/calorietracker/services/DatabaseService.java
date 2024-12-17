c91ecf45a27fa74b1b6aaa3f0e8094bca3bf1589
KianDoyle <kianrk@icloud.com>
2024-12-17 14:39:02 +0000

    Altered tracker

--- src/main/java/com/kd/calorietracker/services/DatabaseService.java
+++ src/main/java/com/kd/calorietracker/services/DatabaseService.java
@@ -1,11 +1,19 @@
 package com.kd.calorietracker.services;
 
+import com.kd.calorietracker.entities.Tracker;
+import com.kd.calorietracker.repositories.TrackerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseService {
 
-    public Tracker getTracker() {
+    private final TrackerRepository trackerRepository;
 
+    public DatabaseService(TrackerRepository trackerRepository) {
+        this.trackerRepository = trackerRepository;
+    }
+
+    public Tracker getTracker() {
+        return trackerRepository.findAll().getFirst();
     }
 }
