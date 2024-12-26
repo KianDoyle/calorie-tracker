d1b8924537be9f0e8969204dd4d0cb489009aa4e
KianDoyle <kianrk@icloud.com>
2024-12-26 02:48:23 +0000

    Added bash scripts

---
+++ bash/app-setup.sh
@@ -0,0 +1,22 @@
+sudo apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+
+sudo apt install -y git
+sudo apt install -y maven
+sudo DEBIAN_FRONTEND=noninteractive apt-get install openjdk-21-jdk -y
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+export DB_HOST="jdbc:mysql://<ip>:3306/library"
+export DB_USER="java-app"
+export DB_PASS="password"
+
+mkdir -p /home/ubuntu/app
+cd /home/ubuntu/app
+
+git clone https://github.com/KianDoyle/calorie-tracker.git
+
+cd calorie-tracker
+
+mvn spring-boot:start
