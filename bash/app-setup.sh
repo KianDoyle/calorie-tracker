6aa0c91ccc27472abd6bfba598bf5dbdd9b568d2
KianDoyle <kianrk@icloud.com>
2024-12-30 21:27:59 +0000

    Updated pom.xml

--- bash/app-setup.sh
+++ bash/app-setup.sh
@@ -3,12 +3,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 sudo apt install -y git
 sudo apt install -y maven
+apt install temurin-21-jdk
 sudo DEBIAN_FRONTEND=noninteractive apt-get install openjdk-21-jdk -y
 
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-export DB_HOST="jdbc:mysql://3.10.214.253:3306/tacker_db"
+export DB_HOST="jdbc:mysql://35.178.90.206:3306/tacker_db"
 export DB_USER="java-app"
 export DB_PASS="password"
 
--- bash/db-setup.sh
+++ bash/db-setup.sh
@@ -6,7 +6,7 @@ sudo sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/
 sudo sed -i "s/^mysqlx-bind-address.*/mysqlx-bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 
-sudo mysql -ppassword -e "CREATE USER 'java-app'@'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON library.* TO 'java-app'@'%'; FLUSH PRIVILEGES;"
+sudo mysql -ppassword -e "CREATE USER 'java-app'@'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON tracker_db.* TO 'java-app'@'%'; FLUSH PRIVILEGES;"
 
 sudo systemctl restart mysql
 
