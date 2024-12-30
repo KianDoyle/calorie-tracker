1587e3e76e4dd84c75416a647f35b4ba04629267
KianDoyle <kianrk@icloud.com>
2024-12-30 15:12:29 +0000

    Final updates

--- bash/app-setup.sh
+++ bash/app-setup.sh
@@ -8,7 +8,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install openjdk-21-jdk -y
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-export DB_HOST="jdbc:mysql://<ip>:3306/library"
+export DB_HOST="jdbc:mysql://3.10.214.253:3306/tacker_db"
 export DB_USER="java-app"
 export DB_PASS="password"
 
